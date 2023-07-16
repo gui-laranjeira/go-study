97d522771335d2d07b370e6e18b588a899483a85
Guilherme Laranjeira <guilr14@gmail.com>
2023-07-16 13:46:12 -0300

    test: saveToFile test implementation

--- 01_cards_game/deck_test.go
+++ 01_cards_game/deck_test.go
@@ -1,24 +1,54 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
-//t.Errorf("Expected deck length of 16, but got %v", len(d))
 func TestNewDeck(t *testing.T) {
 	deck := newDeck()
+
 	if len(deck) != 52 {
 		t.Errorf("Expected deck length of 52, but got %v", len(deck))
+	}
+	if deck[0] != "Ace of Spades" {
+		t.Errorf("Expected first card of Ace of Spades, but got %v", deck[0])
+	}
+	if deck[len(deck)-1] != "King of Clubs" {
+		t.Errorf("Expected first card of King of Clubs, but got %v", deck[len(deck)-1])
+	}
+}
+
+func TestSaveToFile(t *testing.T) {
+	filename := "_decktesting.txt"
+	os.Remove(filename)
+
+	deck := newDeck()
+	deck.saveToFile(filename)
+
+	_, err := os.Stat(filename)
+	if err != nil {
+		t.Errorf("No file found with the name %v", filename)
+		return
 	}
 }
 
 func TestNewDeckFromFile(t *testing.T) {
-	filename := "my_cards.txt"
-	deck, err := newDeckFromFile("my_cards.txt")
+	filename := "_decktesting.txt"
+	deck, err := newDeckFromFile(filename)
+
 	if err != nil {
-		t.Errorf("No file found with the name %s", filename)
+		t.Errorf("No file found with the name %v", filename)
 		return
 	}
 	if len(deck) != 52 {
 		t.Errorf("Expected deck length of 52, but got %v", len(deck))
 		return
+	}
+	if deck[0] != "Ace of Spades" {
+		t.Errorf("Expected first card of Ace of Spades, but got %v", deck[0])
+	}
+	if deck[len(deck)-1] != "King of Clubs" {
+		t.Errorf("Expected first card of King of Clubs, but got %v", deck[len(deck)-1])
 	}
 }
--- 01_cards_game/main.go
+++ 01_cards_game/main.go
@@ -1,20 +1,27 @@
 package main
 
-import "log"
+import (
+	"log"
+)
 
 func main() {
-	// cards := newDeck()
+	filename := "my_cards.txt"
+	cards := newDeck()
 
-	// err := cards.saveToFile("my_cards.txt")
-	// if err != nil {
-	// 	log.Panic(err)
-	// }
-	newDeck, err := newDeckFromFile("my_cards.txt")
+	err := cards.saveToFile(filename)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	newDeck, err := newDeckFromFile(filename)
 	if err != nil {
 		log.Flags()
 	}
+
 	newDeck.print()
+
 	newDeck.shuffle()
 	println("\n\n Shuffled cards:\n")
+
 	newDeck.print()
 }
